89a56004b68129e27b2a0250e8e49c0517c134e2
Luigi Blasquez <luigiblasquez@gmail.com>
2024-10-12 07:31:59 +0000

    renaming some stuff, also adding a function to make it actually function

--- src/spooky/spooky/rgb.py
+++ src/spooky/spooky/rgb.py
@@ -1,19 +1,24 @@
 import can
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
+# from std_msgs.msg import String
 
 
 class rgb(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        super().__init__('rgb_leds')
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
         self.bus = can.interface.Bus(interface='socketcan', channel='vcan0', bitrate='500000')
+
+    def can_publish(self, arbitration_id, data, is_extended_id) -> None:
+        can_msg = can.Message(
+                arbitration_id = arbitration_id,
+                data = data, 
+                is_extended_id = is_extended_id
+                ) 
+        self.bus.send(can_msg)
 
     # method to convert to INT16 Big Endian (i hope)
     # also returns two variables for (AB)
