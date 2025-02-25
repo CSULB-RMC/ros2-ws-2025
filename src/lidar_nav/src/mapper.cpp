377f21dcc03ea002af52da62fa81a3a57284d5a8
porterclev <porterclev@gmail.com>
2025-02-24 16:37:19 -0800

    merge 2

--- src/lidar_nav/src/mapper.cpp
+++ src/lidar_nav/src/mapper.cpp
@@ -19,43 +19,42 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/time.hpp"
 
-
 class Mapper : public rclcpp::Node
 {
 public:
   Mapper()
-  : Node("mapper")
+      : Node("mapper")
   {
     auto topic_callback =
-      [this](sensor_msgs::msg::PointCloud2::SharedPtr msg) -> void {
-        // RCLCPP_INFO(this->get_logger(), std::to_string(msg->data.));
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        [this](sensor_msgs::msg::PointCloud2::SharedPtr msg) -> void
+    {
+      // RCLCPP_INFO(this->get_logger(), std::to_string(msg->data.));
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-        pcl::fromROSMsg(*msg, *cloud); // Convert ROS PointCloud2 to PCL
+      pcl::fromROSMsg(*msg, *cloud); // Convert ROS PointCloud2 to PCL
 
-        // std::cout << cloud->points[0].x << std::endl;
-         for (const auto& point : cloud->points) {
-          std::cout << point << std::endl;
-         }
+      // std::cout << cloud->points[0].x << std::endl;
+      for (const auto &point : cloud->points)
+      {
+        std::cout << point << std::endl;
+      }
 
-        // const auto& point = *cloud;
-        // std::cout << "x: " << point.x << ", y: " << point.y << ", z: " << point.z << std::endl;
-
-      };
+      // const auto& point = *cloud;
+      // std::cout << "x: " << point.x << ", y: " << point.y << ", z: " << point.z << std::endl;
+    };
     subscription_ =
-      this->create_subscription<sensor_msgs::msg::PointCloud2>("unilidar/cloud", 10, topic_callback);
+        this->create_subscription<sensor_msgs::msg::PointCloud2>("unilidar/cloud", 10, topic_callback);
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
-  
-  int rows = 200;
-  int cols = 200;
-  std::vector<std::vector<int>> grid(rows, std::vector<int>(cols, 0))
 
+  // int rows = 200;
+  // int cols = 200;
+  // std::vector<std::vector<int>> grid(rows, std::vector<int>(cols, 0))
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Mapper>());
