8d7fc0b24af3d290943ad6253781b4fbc4e42d59
Leo Pan <leo.long.pan@gmail.com>
2025-02-26 02:24:38 +0000

    logs msg and pointXYZ data in two csv files

--- src/lidar_nav/src/mapper.cpp
+++ src/lidar_nav/src/mapper.cpp
@@ -1,12 +1,12 @@
 #include <memory>
-#include "iostream"
+#include <iostream>
 #include <string>
 #include <stdio.h>
 #include <iterator>
 #include <algorithm>
 #include <chrono>
 #include <cmath>
-#include <vector>
+#include <fstream>
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
@@ -19,39 +19,47 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/time.hpp"
 
-
 class Mapper : public rclcpp::Node
 {
 public:
   Mapper()
   : Node("mapper")
   {
+
+    pointsOutput.open("points.csv");
+    msgOutput.open("message.csv");
+    pointsOutput << "x,y,z" << std::endl;
+    msgOutput << "width,height,is_dense" << std::endl;
+
     auto topic_callback =
       [this](sensor_msgs::msg::PointCloud2::SharedPtr msg) -> void {
         // RCLCPP_INFO(this->get_logger(), std::to_string(msg->data.));
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
         pcl::fromROSMsg(*msg, *cloud); // Convert ROS PointCloud2 to PCL
+        msgOutput << cloud->width << "," << cloud->height << "," << cloud->is_dense << std::endl;
 
-        // std::cout << cloud->points[0].x << std::endl;
          for (const auto& point : cloud->points) {
+          pointsOutput << point.x << "," << point.y << "," << point.z << std::endl;
           std::cout << point << std::endl;
          }
-
-        // const auto& point = *cloud;
-        // std::cout << "x: " << point.x << ", y: " << point.y << ", z: " << point.z << std::endl;
-
       };
     subscription_ =
       this->create_subscription<sensor_msgs::msg::PointCloud2>("unilidar/cloud", 10, topic_callback);
+
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+  std::ofstream pointsOutput;
+  std::ofstream msgOutput;
+
   
-  int rows = 200;
-  int cols = 200;
-  std::vector<std::vector<int>> grid(rows, std::vector<int>(cols, 0))
+  // int unit = 15; //centimeters
+  // int starting_grid = 6 //meters
+  // int rows = starting_grid / unit / 100;
+  // int cols = 200;
+  // int grid[rows][cols]
 
 };
 
