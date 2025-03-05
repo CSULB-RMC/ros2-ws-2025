289c667fd77c9926565b24dcd784239c6ed4581e
Leo Pan <leo.long.pan@gmail.com>
2025-03-05 20:21:40 +0000

    quaternion conversion into euler angles.

--- src/lidar_nav/src/mapper.cpp
+++ src/lidar_nav/src/mapper.cpp
@@ -7,6 +7,8 @@
 #include <chrono>
 #include <cmath>
 #include <fstream>
+#include <cmath>
+#include <math.h>
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
@@ -22,6 +24,41 @@
 class Mapper : public rclcpp::Node
 {
 public:
+  struct MyEuler {
+    float x;
+    float y;
+    float z;
+  };
+
+  MyEuler QuatToEuler(float x, float y, float z, float w) {
+    float t0 = 2.0 * (w * x + y * z);
+    float t1 = 1.0 - 2.0 * (x * x + y * y);
+    float roll_x = atan2(t0, t1);
+
+    float t2 = 2.0 * (w * y - z * x);
+    if(t2 > 1){
+      t2 = 1.0;
+    } else if(t2 < -1){
+      t2 = -1.0;
+    }
+    float pitch_y = asin(t2);
+
+    float t3 = 2.0 * (w * z + x * y);
+    float t4 = 1.0 -2.0 * (y * y + z * z);
+    float yaw_z = atan2(t3, t4);
+
+    roll_x = roll_x * 180 / M_PI;
+    pitch_y = pitch_y * 180 / M_PI;
+    yaw_z = yaw_z * 180 / M_PI;
+    
+    MyEuler results;
+    results.x = roll_x;
+    results.y = pitch_y;
+    results.z = yaw_z;
+
+    return results;
+  }
+
   Mapper()
   : Node("mapper")
   {
@@ -31,26 +68,40 @@ public:
     pointsOutput << "x,y,z" << std::endl;
     msgOutput << "width,height,is_dense" << std::endl;
 
-    auto topic_callback =
+
+
+
+    auto cloud_callback =
       [this](sensor_msgs::msg::PointCloud2::SharedPtr msg) -> void {
+        
         // RCLCPP_INFO(this->get_logger(), std::to_string(msg->data.));
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-        pcl::fromROSMsg(*msg, *cloud); // Convert ROS PointCloud2 to PCL
-        msgOutput << cloud->width << "," << cloud->height << "," << cloud->is_dense << std::endl;
+        // pcl::fromROSMsg(*msg, *cloud); // Convert ROS PointCloud2 to PCL
+        // msgOutput << cloud->width << "," << cloud->height << "," << cloud->is_dense << std::endl;
 
-         for (const auto& point : cloud->points) {
-          pointsOutput << point.x << "," << point.y << "," << point.z << std::endl;
-          std::cout << point << std::endl;
-         }
+        //  for (const auto& point : cloud->points) {
+          // pointsOutput << point.x << "," << point.y << "," << point.z << std::endl;
+          // std::cout << point << std::endl;
+        //  }
       };
-    subscription_ =
-      this->create_subscription<sensor_msgs::msg::PointCloud2>("unilidar/cloud", 10, topic_callback);
+
+    auto imu_callback =
+      [this](sensor_msgs::msg::Imu::SharedPtr msg) -> void {
+        MyEuler rotations = QuatToEuler(msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
+        std::cout << rotations.x  << ", " << rotations.y << ", " << rotations.z << std::endl;
+      };
+
+    cloud_subscription_ =
+      this->create_subscription<sensor_msgs::msg::PointCloud2>("unilidar/cloud", 10, cloud_callback);
+    imu_subscription_ = 
+      this->create_subscription<sensor_msgs::msg::Imu>("unilidar/imu", 10, imu_callback);
 
   }
 
 private:
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
   std::ofstream pointsOutput;
   std::ofstream msgOutput;
 
