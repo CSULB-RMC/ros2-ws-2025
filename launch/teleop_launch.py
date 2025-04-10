043bf91146f0b655f7ce2933d1a1054f094eabec
porterclev <porterclev@gmail.com>
2025-04-10 11:04:16 -0700

    cleaned up launch files

--- launch/teleop_launch.py
+++ launch/teleop_launch.py
@@ -3,7 +3,21 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+'''
+This is the launch file for the teleop computer controlling the rover.
+package:
+    node: <script_alias>
+    parameters: ["<parameter_file>"]
 
+joy:    # ros library node for handling joystick inputs
+    joy_node: "joy_node"
+    parameters: [autorepeat_rate: 0.0]  # disables auto-repeat
+    
+teleop:
+    controller: "controller"
+    parameters: ["teleop/config/controller.yaml"]
+...
+'''
 def generate_launch_description():
     ld = LaunchDescription()
     controllerConfig = os.path.join(
