be4608510cd9bdea15389e1e2fd13bbf9a76727b
porterclev <porterclev@gmail.com>
2025-04-10 18:29:43 -0700

    updated scripts and launch files

--- scripts/local-teleop-can-test.sh
+++ scripts/local-teleop-can-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "$(pwd)/install/local_setup.sh"
-ros2 launch $(pwd)/launch/teleop_launch.py > /dev/null 2>&1 &
-$(pwd)/scripts/launch.sh
+"$(pwd)/scripts/can-setup.sh"
+ros2 launch "$(pwd)/launch/local_teleop_launch.py"
